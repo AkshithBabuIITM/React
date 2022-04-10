57fcc5024429bfcce8c2decb194f656c69d45639
CEA-Webops-Team <ce19b063@smail.iitm.ac.in>
2022-04-10 12:29:13 +0530

    react

--- reat/src/App.js
+++ reat/src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          AKSHITH 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React.js</h1>
+      <p>
+        Webdev Intern Vakil Search
+      </p>
+        
+     
     </div>
   );
 }
